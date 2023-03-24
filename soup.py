af40562ad3c3bde8f0311e5cce110e9d29acb538
powerlego <11838540+powerlego@users.noreply.github.com>
2023-03-24 12:12:29 -0400

    update

--- soup.py
+++ soup.py
@@ -7,8 +7,10 @@ from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from seleniumwire.utils import decode
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 options = webdriver.ChromeOptions()
+# options.add_argument("--auto-open-devtools-for-tabs")
 
 urls = [
     "https://{company_name}.com/investor-relations",
@@ -30,13 +32,7 @@ keywords = ["earnings", "conference", "call"]
 ir_links = []
 
 
-def getWebsite(driver, url, company_name, count=0):
-    if count > 0:
-        print("retrying")
-        driver.quit()
-        time.sleep(5)
-        driver = webdriver.Chrome(options=options)
-
+def getWebsite(driver, url, company_name, ticker, count=0):
     driver.set_page_load_timeout(30)
     try:
         driver.get(url)
@@ -48,37 +44,59 @@ def getWebsite(driver, url, company_name, count=0):
                 if find_param in request.url:
                     if request.response.headers["Content-Type"]:
                         if "text/html" in request.response.headers["Content-Type"]:
-                            sites.append(request)
+                            if "track" not in request.url:
+                                sites.append(request)
 
         del driver.requests
 
         for site in sites:
             if site.response.status_code == 200:
-                try:
-                    body = decode(
-                        site.response.body,
-                        site.response.headers.get("Content-Encoding", "identity"),
-                    )
-                    content_type = site.response.headers["Content-Type"]
+                if "404" in site.url:
+                    return ""
+                body = decode(
+                    site.response.body,
+                    site.response.headers.get("Content-Encoding", "identity"),
+                )
+                content_type = site.response.headers["Content-Type"]
+                splt = content_type.split("charset=")
+                if len(splt) > 1:
                     encoding = content_type.split("charset=")[1]
                     body = str(body, encoding)
-                    search = re.search("(?:I|i)nvestors? (?:R|r)elations?", body)
+                    if "page not found" in body.lower():
+                        return ""
+                    search = re.search("(?:E|e)arnings?", body)
                     if search != None:
                         return site.url
                     else:
-                        search = re.search("(?:I|i)nvestors?", body)
+                        search = re.search("Q(?:1|2|3|4)", body)
                         if search != None:
                             return site.url
-                except:
-                    pass
+                        else:
+                            search = re.search("(?:F|f)iscal", body)
+                            if search != None:
+                                return site.url
+                            else:
+                                search = re.search("(?:S|s)tock", body)
+                                if search != None:
+                                    return site.url
+                                else:
+                                    search_str = f"(?:NYSE|NASDAQ):\s?{ticker}"
+                                    print(search_str)
+                                    search = re.search(search_str, body)
+                                    if search != None:
+                                        return site.url
+
         return ""
-    except:
+    except TimeoutException:
         print("timeout")
-        if count > 5:
-            driver = webdriver.Chrome(options=options)
+        if count >= 5:
             return ""
         count = count + 1
-        return getWebsite(driver, url, company_name, count)
+        print("retrying")
+        return getWebsite(driver, url, company_name, ticker, count)
+    except Exception as e:
+        print(e)
+        return ""
 
 
 good_urls = dict()
@@ -89,30 +107,48 @@ with open("companies.csv") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         company_url = row[1]
+        ticker = row[2]
         if company_url != "":
-            search = re.search("(?:www\.)(.*?)\.", company_url)
-            if search != None:
-                driver = webdriver.Chrome(options=options)
-                company_name = search.group(1)
-                found_company = False
-                for url in urls:
-                    format_url = url.replace("{company_name}", company_name)
-                    print(format_url)
-                    result = getWebsite(driver, format_url, company_name)
-                    if result != "":
-                        good_urls[row[0]] = result
-                        found_company = True
-                        break
-                    # driver.find_element(by=)
+            if ".com" in company_url:
+                search = re.search("(?:www\.)(.*?)\.", company_url)
+                if search != None:
+                    driver = webdriver.Chrome(options=options)
+                    company_name = search.group(1)
+                    found_company = False
 
-                if found_company == False:
-                    missing_urls.append(row[0])
-                driver.quit()
-                time.sleep(5)
-        #
-        # print(search)
-        # company_name = search.group(1)
-        # print(company_name)
+                    for url in urls:
+                        format_url = url.replace("{company_name}", company_name)
+                        print(format_url)
+                        result = getWebsite(driver, format_url, company_name, ticker)
+                        if result != "":
+                            good_urls[row[0]] = result
+                            found_company = True
+                            break
+                        # driver.find_element(by=)
+
+                    if found_company == False:
+                        missing_urls.append(row[0])
+                    driver.quit()
+            else:
+                search = re.search("(?:www\.)(.*?)\.(.*?)\/", company_url)
+                if search != None:
+                    driver = webdriver.Chrome(options=options)
+                    company_name = search.group(1)
+                    extension = search.group(2)
+                    found_company = False
+
+                    for url in urls:
+                        format_url = url.replace("{company_name}", company_name)
+                        format_url = format_url.replace(".com", "." + extension)
+                        result = getWebsite(driver, format_url, company_name, ticker)
+                        if result != "":
+                            good_urls[row[0]] = result
+                            found_company = True
+                            break
+
+                    if found_company == False:
+                        missing_urls.append(row[0])
+                    driver.quit()
 
 
 print("Good URLs")
